80dba4cc0ee2fc2f9e96983287ca7c71eccae0e7
roman svitukha <griind@gmail.com>
2017-12-04 21:26:17 -0600

    rs Added QuickSort

--- i04.Web/Controllers/HomeController.cs
+++ i04.Web/Controllers/HomeController.cs
@@ -58,5 +58,36 @@ namespace i04.Web.Controllers
 
             return View(number);
         }
+        public void  QuickSort(int[] arr, int start, int end)
+        {
+            if (start < end)
+            {
+                int bordPos = Partition(arr, start, end);
+                QuickSort(arr,start, bordPos - 1);
+                QuickSort(arr, bordPos + 1, end);
+            }
+        }
+        private int Partition(int[] arr, int start, int end)
+        {
+            int i = start + 1;
+            int border = arr[start];
+            for (int j = start + 1; j <= end; j++)
+            {
+                if (arr[j] < border)
+                {
+                    Swap(ref arr[i], ref arr[j]);
+                }
+            }
+            Swap(ref arr[start], ref arr[i - 1]);
+            return i - 1;
+
+        }
+
+        private void Swap(ref int v1, ref int v2)
+        {
+            int tmp = v1;
+            v1 = v2;
+            v2 = tmp;
+        }
     }
 }
