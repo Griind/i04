b313dba42ea34b0181fed986942aec8c4468b06e
roman svitukha <griind@gmail.com>
2018-07-11 00:54:37 -0500

    Added 1rst iteration of the encoder (currently only to base64)

---
+++ i04.Web/Controllers/EncDecController.cs
@@ -0,0 +1,28 @@
+﻿using i04.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace i04.Web.Controllers
+{
+    public class EncDecController : Controller
+    {
+        // GET: EncDec
+        public ActionResult EncDec()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Encode(EncodeData d)
+        {
+            if (d.DataLeft != null)
+            {
+                d.DataRight = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(d.DataLeft));
+            }
+
+            return View("EncDec",d);
+        }
+    }
+}
---
+++ i04.Web/Models/EncodeData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace i04.Web.Models
+{
+    public class EncodeData
+    {
+        public string DataLeft { get; set; }
+        public string DataRight { get; set; }
+    }
+}
